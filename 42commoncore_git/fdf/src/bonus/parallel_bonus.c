d801cdb9abc51dee22d3e788ed97b25d17bbbb28
Vedat Zeybek <vzeybek@k1m33s01.42istanbul.com.tr>
2025-08-01 19:47:16 +0300

    m

---
+++ 42commoncore_git/fdf/src/bonus/parallel_bonus.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parallel_bonus.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vzeybek <vzeybek@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/01 08:15:30 by vzeybek           #+#    #+#             */
+/*   Updated: 2025/08/01 08:15:31 by vzeybek          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../inc/fdf_bonus.h"
+
+static void	parallel_projection(t_point *point, t_fdf *fdf)
+{
+	double	x_proj;
+	double	y_proj;
+	double	center_x;
+	double	center_y;
+	double	scale;
+
+	scale = fdf->scale;
+	center_x = (fdf->col - 1) / 2.0;
+	center_y = (fdf->row - 1) / 2.0;
+	x_proj = (double)point->x - center_x;
+	y_proj = (double)point->y - center_y - point->z * 0.2;
+	point->screen_x = (int)(fdf->offset_x + x_proj * scale);
+	point->screen_y = (int)(fdf->offset_y + y_proj * scale);
+}
+
+void	apply_parallel_projection(t_fdf *fdf)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < fdf->row)
+	{
+		j = 0;
+		while (j < fdf->col)
+		{
+			parallel_projection(&(fdf->points[i][j]), fdf);
+			j++;
+		}
+		i++;
+	}
+}
---
+++ 42commoncore_git/fdf/src/mandatory/create_points.c
@@ -0,0 +1,119 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   create_points.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vzeybek <vzeybek@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/01 08:14:38 by vzeybek           #+#    #+#             */
+/*   Updated: 2025/08/01 11:56:47 by vzeybek          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../inc/fdf.h"
+
+int	count_column(char **map)
+{
+	int		col;
+	char	**first_row_split;
+
+	first_row_split = ft_split(map[0], ' ');
+	col = 0;
+	while (first_row_split[col])
+		col++;
+	free_split(first_row_split);
+	return (col);
+}
+
+int	count_rows(char *filename)
+{
+	int		fd;
+	int		count;
+	char	*line;
+
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
+	{
+		exit(EXIT_FAILURE);
+	}
+	count = 0;
+	line = get_next_line(fd);
+	while (line != NULL)
+	{
+		count++;
+		free(line);
+		line = get_next_line(fd);
+	}
+	close(fd);
+	return (count);
+}
+
+char	**read_map(char *filename, int rows)
+{
+	int		fd;
+	char	**map;
+	int		i;
+
+	fd = open(filename, O_CREAT | O_RDONLY, 0644);
+	i = 0;
+	map = malloc(sizeof(char *) * (rows + 1));
+	if (!map)
+		return (NULL);
+	while (i < rows)
+	{
+		if (!map)
+			return (NULL);
+		map[i] = get_next_line(fd);
+		i++;
+	}
+	map[i] = NULL;
+	close(fd);
+	return (map);
+}
+
+static void	create_row_points(t_point *row_points, char *map_line,
+			int col, int row_idx)
+{
+	char	**split;
+	int		j;
+	char	**parts;
+
+	split = ft_split(map_line, ' ');
+	j = 0;
+	while (j < col)
+	{
+		parts = ft_split(split[j], ',');
+		row_points[j].x = j;
+		row_points[j].y = row_idx;
+		row_points[j].z = ft_atoi(parts[0]);
+		if (parts[1])
+			row_points[j].color = ft_atoi_base(parts[1], 16);
+		else
+			row_points[j].color = get_color(row_points[j].z);
+		row_points[j].screen_x = 0;
+		row_points[j].screen_y = 0;
+		free_split(parts);
+		j++;
+	}
+	free_split(split);
+}
+
+t_point	**create_points(char **map, int col, int row)
+{
+	t_point	**points;
+	int		i;
+
+	points = malloc(sizeof(t_point *) * (row + 1));
+	if (!points)
+		return (NULL);
+	i = 0;
+	while (i < row)
+	{
+		points[i] = malloc(sizeof(t_point) * (col));
+		if (!points[i])
+			return (NULL);
+		create_row_points(points[i], map[i], col, i);
+		i++;
+	}
+	return (points);
+}
